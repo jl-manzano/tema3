5bf61724b3f8a94167173061438e698e269562ba
jl-manzano <jl.manzano@iesnervion.es>
2024-11-13 11:51:29 +0100

    a

---
+++ Tema3/src/boletin3/Ejer2.java
@@ -0,0 +1,23 @@
+package boletin3;
+
+import java.util.Scanner;
+
+public class Ejer2 {
+	public static void main(String[] args) {
+		
+		// crear array 4 filas x 5 columnas
+		int notas[][] = new int[4][5];
+		
+		// crear scanner
+		Scanner sc = new Scanner(System.in);
+		
+		// variable para almacenar nota introducida por usuario
+		int nota;
+		
+		for (int i = 0; i < notas.length; i++) {
+			
+			System.out.println("Alumno " + (i+1) + ": ");
+		}
+		
+	}
+}
